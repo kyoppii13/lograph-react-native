5bb40fe70537ca8d09a5c15fd2ac35052ce645ff
Kyou13 <kyou1013@gmail.com>
2020-08-05 00:20:37 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+extends: [
+"eslint:recommended",
+"plugin:react/recommended",
+"plugin:@typescript-eslint/recommended",
+"plugin:prettier/recommended",
+"prettier/@typescript-eslint",
+"prettier/react",
+],
+plugins: ["react", "@typescript-eslint"],
+rules: {},
+}
