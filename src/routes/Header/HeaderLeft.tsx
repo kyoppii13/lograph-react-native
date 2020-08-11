d998b5e3de8d63a0047038f710988a859486aacb
Kyou13 <kyou1013@gmail.com>
2020-08-12 01:13:48 +0900

    update icons on HeaderLeft

--- src/routes/Header/HeaderLeft.tsx
+++ src/routes/Header/HeaderLeft.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { TouchableOpacity, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { DrawerActions } from "@react-navigation/routers";
+import { COLOR } from "../../constants/theme";
 
 export default function HeaderLeft() {
   const { dispatch } = useNavigation();
@@ -9,8 +11,11 @@ export default function HeaderLeft() {
     dispatch(DrawerActions.openDrawer());
   }, [dispatch]);
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>open</Text>
-    </TouchableOpacity>
+    <Icon.Button
+      name="bars"
+      color={COLOR.WHITE}
+      backgroundColor={COLOR.MAIN}
+      onPress={onPress}
+    />
   );
 }
